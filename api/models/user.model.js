dd852409dd6129bbe78465088611abad73295807
ishh91 <ishantsharma9105@gmail.com>
2024-02-03 09:59:25 +0530

    Env

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -2,51 +2,51 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  username:{
-    type:String,
-    require:true,
-    unique:true
+  username: {
+    type: String,
+    require: true,
+    unique: true
   },
-  email:{
-    type:String,
-    require:true,
-    unique:true
+  email: {
+    type: String,
+    require: true,
+    unique: true
   },
-  password:{
-    type:String,
-    require:true,
+  password: {
+    type: String,
+    require: true,
   },
-  img:{
-    type:String,//store url
-    require:false,
+  img: {
+    type: String,//store url
+    require: false,
   },
-  country:{
-    type:String,
-    require:true,
+  country: {
+    type: String,
+    require: true,
   },
-  phone:{
-    type:String,
-    require:false,
+  phone: {
+    type: String,
+    require: false,
   },
-  password:{
-    type:String,
-    require:true,
+  password: {
+    type: String,
+    require: true,
   },
-  desc:{
-    type:String,
-    require:false,
+  desc: {
+    type: String,
+    require: false,
   },
-  isSeller:{
-    type:Boolean,
-    default:false,
-    require:true,
+  isSeller: {
+    type: Boolean,
+    default: false,
+    require: true,
   },
-  verified:{
-    type:Boolean,
-    default:false,
+  verified: {
+    type: Boolean,
+    default: false,
   },
-},{
-timestamps:true
+}, {
+  timestamps: true
 });
 
-export default mongoose.model("user",userSchema)
+export default mongoose.model("user", userSchema)
